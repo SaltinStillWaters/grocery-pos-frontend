fce1781155b6fcdb08e7e2d6cf785cf12c0980e6
SaltinStillWaters <SaltinStillWaters@gmail.com>
2025-12-03 20:27:53 +0800

    feat: styled LoginForm

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,58 +1,92 @@
-import axios from "axios"
-import { useEffect, useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getUser, useCurrentUser } from "../utils";
 
 export default function LoginForm() {
-    useCurrentUser(true);
+  useCurrentUser(true);
+  const navigate = useNavigate();
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [show, setShow] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState("");
 
-    const navigate = useNavigate();
-    
-    async function handleLogin() {
-        try {
-            const res = await axios.post(
-                `http://localhost:3000/auth/login`, 
-                { username, password },
-                { withCredentials: true }
-            );
+  async function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
+    setError("");
 
-            console.log({res});
-            navigate('/dashboard')
-        } catch (err: any) {
-            if (err.response) {
-                console.log(err.response.data.message);
-            } else {
-                console.error("Network error", err);
-            }
-        }
+    try {
+      const res = await axios.post(
+        `http://localhost:3000/auth/login`,
+        { username, password },
+        { withCredentials: true }
+      );
+
+      console.log({ res });
+      navigate("/dashboard");
+    } catch (err: any) {
+      if (err.response) {
+        setError(err.response.data.message);
+      } else {
+        console.error("Network error", err);
+        setError("Network Error, please try again");
+      }
     }
+  }
 
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+      <div
+        className="card shadow p-4"
+        style={{ width: "380px", borderRadius: "12px" }}
+      >
+        <h3 className="text-center mb-4 fw-semibold">Welcome Back</h3>
 
-    return (
-    <>
-        <input
-            type='text'
-            placeholder='username'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-            type={show ? 'text' : 'password'}
-            placeholder='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        />
-        <input
-            type='checkbox'
-            onChange={() => setShow(!show)}
-        />
-        <button onClick={handleLogin}>
+        {error && (
+          <div className="alert alert-danger py-2">
+            {Array.isArray(error)
+              ? error.map((msg, i) => <div key={i}>{msg}</div>)
+              : error}
+          </div>
+        )}
+
+        <form onSubmit={handleLogin}>
+          <div className="mb-3">
+            <label className="form-label">Username</label>
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input
+              type={show ? "text" : "password"}
+              className="form-control form-control-lg"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="form-check mb-3">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              checked={show}
+              onChange={() => setShow(!show)}
+            />
+            <label className="form-check-label">Show password</label>
+          </div>
+
+          <button type="submit" className="btn btn-primary btn-lg w-100">
             Login
-        </button>
-    </>
-    )
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -10,59 +10,89 @@ export default function Users() {
   const [users, setUsers] = useState<User[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    async function fetchUsers() {
-      try {
-        const res = await axios.get(`${API_URL}/users`, {
-          withCredentials: true,
-        });
-        setUsers(res.data.data);
-      } catch (err) {
-        console.error("Failed to load users", err);
-        setUsers([]);
-      } finally {
-        setLoading(false);
-      }
+  async function fetchUsers() {
+    setLoading(true);
+    try {
+      const res = await axios.get(`${API_URL}/users`, {
+        withCredentials: true,
+      });
+      setUsers(res.data.data);
+    } catch (err) {
+      console.error("Failed to load users", err);
+      setUsers([]);
+    } finally {
+      setLoading(false);
     }
+  }
 
+  useEffect(() => {
     fetchUsers();
   }, []);
 
   const [changes, setChanges] = useState<Record<string, Partial<User>>>({});
 
-  function handleChange(
-    user_id: string, value: any, property: keyof User
-    ) {
-      setChanges((prev) => {
-        const userChanges = { ...prev[user_id], [property]: value };
-        if (value === '' || value === null)
-            delete userChanges[property];
+  function handleChange(user_id: string, value: any, property: keyof User) {
+    setChanges((prev) => {
+      const userChanges = { ...prev[user_id], [property]: value };
+      if (value === "" || value === null) delete userChanges[property];
 
-        const newChanges = { ...prev, [user_id]: userChanges };
-        if (Object.keys(userChanges).length === 0)
-            delete newChanges[user_id];
+      const newChanges = { ...prev, [user_id]: userChanges };
+      if (Object.keys(userChanges).length === 0) delete newChanges[user_id];
 
-        return newChanges;
-    });
-    
-    console.log({
-        ...changes,
-        [user_id]: {
-            ...changes[user_id],
-            [property]: value
-        }
+      return newChanges;
     });
   }
 
-  const [editingCell, setEditingCell] = useState<{ userId: string; property: keyof User } | null>(null);
+  async function handleSave(changes: Record<string, Partial<User>>) {
+    setLoading(true);
+    const updates = Object.entries(changes).map(([_id, update]) => ({
+      _id,
+      update,
+    }));
 
+    try {
+      const res = await axios.patch(
+        `${API_URL}/users`,
+        { updates },
+        { withCredentials: true }
+      );
+      await fetchUsers();
+      console.log({ res });
+    } catch (err) {
+      console.log({ err });
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function handleCancel() {
+    setLoading(true);
+    setChanges({});
+    fetchUsers();
+  }
+
+  const [editingCell, setEditingCell] = useState<{
+    userId: string;
+    property: keyof User;
+  } | null>(null);
 
   if (loading) return <p>Loading users...</p>;
 
   return (
     <div className="container mt-4">
       <h2>Users</h2>
-
+      <button
+        onClick={() => handleSave(changes)}
+        disabled={loading || !Object.keys(changes).length}
+      >
+        Save
+      </button>
+      <button
+        onClick={handleCancel}
+        disabled={loading || !Object.keys(changes).length}
+      >
+        Cancel
+      </button>
       {users && users.length > 0 ? (
         <table className="table table-striped table-bordered">
           <thead>
@@ -76,32 +106,38 @@ export default function Users() {
           <tbody>
             {users.map((u) => (
               <tr key={u._id}>
-                <td onClick={() => setEditingCell({ userId: u._id, property: 'name' })}>
-                    <EditableCell 
-                        value={changes[u._id]?.['name'] ?? u.name}
-                        editing={editingCell?.userId === u._id && editingCell.property === 'name'}
-                        onChange={e => handleChange(u._id, e, 'name')}
-                        onEnd={() => setEditingCell(null)}
-                    />
+                <td
+                  onClick={() =>
+                    setEditingCell({ userId: u._id, property: "name" })
+                  }
+                >
+                  <EditableCell
+                    value={changes[u._id]?.["name"] ?? u.name}
+                    editing={
+                      editingCell?.userId === u._id &&
+                      editingCell.property === "name"
+                    }
+                    onChange={(e) => handleChange(u._id, e, "name")}
+                    onEnd={() => setEditingCell(null)}
+                  />
                 </td>
                 <td>
-                    {
-                        editingCell?.userId === u._id && editingCell.property === 'roles' ?
-                        (
-                            <RolesEdit
-                            roles={changes[u._id]?.['roles'] ?? u.roles}
-                            allRoles={['guest', 'clerk', 'owner', 'unauthenticated']}
-                            onChange={roles => handleChange(u._id, roles, "roles")}
-                            onBlur={() => setEditingCell(null)}
-                            />
-                        ) :
-                        (
-                            <RolesDisplay
-                                roles={changes[u._id]?.['roles'] ?? u.roles}
-                                onClick={() => setEditingCell({ userId: u._id, property: 'roles'})}
-                            />
-                        )
-                    }
+                  {editingCell?.userId === u._id &&
+                  editingCell.property === "roles" ? (
+                    <RolesEdit
+                      roles={changes[u._id]?.["roles"] ?? u.roles}
+                      allRoles={["guest", "clerk", "owner", "unauthenticated"]}
+                      onChange={(roles) => handleChange(u._id, roles, "roles")}
+                      onBlur={() => setEditingCell(null)}
+                    />
+                  ) : (
+                    <RolesDisplay
+                      roles={changes[u._id]?.["roles"] ?? u.roles}
+                      onClick={() =>
+                        setEditingCell({ userId: u._id, property: "roles" })
+                      }
+                    />
+                  )}
                 </td>
                 <td>
                   <input
@@ -112,14 +148,21 @@ export default function Users() {
                     }
                   />
                 </td>
-                <td onClick={() => setEditingCell({ userId: u._id, property: 'password' })}>
-                    <EditableCell 
-                        type="password"
-                        value={changes[u._id]?.['password'] ?? ''}
-                        editing={editingCell?.userId === u._id && editingCell.property === 'password'}
-                        onChange={e => handleChange(u._id, e, 'password')}
-                        onEnd={() => setEditingCell(null)}
-                    />
+                <td
+                  onClick={() =>
+                    setEditingCell({ userId: u._id, property: "password" })
+                  }
+                >
+                  <EditableCell
+                    type="password"
+                    value={changes[u._id]?.["password"] ?? ""}
+                    editing={
+                      editingCell?.userId === u._id &&
+                      editingCell.property === "password"
+                    }
+                    onChange={(e) => handleChange(u._id, e, "password")}
+                    onEnd={() => setEditingCell(null)}
+                  />
                 </td>
               </tr>
             ))}
